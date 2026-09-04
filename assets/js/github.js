0030cd09e0c890685d2805dbcdc690cd29834a98
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-09-04 17:03:53 +0800

    chore: bump version to v2.12.3

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,4 +1,4 @@
-import { getVersion, setVersion } from './version.js?v=2.11.3';
+import { getVersion, setVersion } from './version.js?v=2.12.3';
 
 const API_BASE = 'https://chi-merch-api.tsai97216.workers.dev';
 const REPO_OWNER = 'tsai97216';
